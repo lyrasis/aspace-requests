6bf5c03315fae567053c807b9a37109c5218cf22
Mark Cooper <markchristophercooper@gmail.com>
2018-02-09 09:02:22 -0800

    Set random request user password if not defined

--- backend/plugin_init.rb
+++ backend/plugin_init.rb
@@ -2,6 +2,10 @@
 ArchivesSpaceService.loaded_hook do
   Log.info "\n\n\n~~~ ASPACE REQUESTS PLUGIN ENABLED ~~~\n\n\n"
 
+  unless AppConfig.has_key? :request_user_secret
+    AppConfig[:request_user_secret] = SecureRandom.hex
+  end
+
   ArchivesSpaceService.create_hidden_system_user(
     "request_user", "Request System User", AppConfig[:request_user_secret]
   )
